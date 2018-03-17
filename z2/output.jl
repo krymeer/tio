f040e5f2b0f058a5675202987a1f2d5f664c8abe
krymeer <krymeer@gmail.com>
2018-03-17 16:28:56 +0100

    Enabled charts creating with the usage of Gadfly

--- z2/output.jl
+++ z2/output.jl
@@ -1,16 +1,18 @@
-function createchart(bvarray, wvarray, avarray)
-  println("BEST")
-  println(bvarray)
+using DataFrames, Gadfly
 
-  println("\nWORST")
-  println(wvarray)
+# Generowanie wykresu na podstawie uzyskanych wynikow. Przedstawiane sa wartosci najlepszego
+# i najgorszego rozwiazania z danej populacji, a takze srednia calej generacji
+function createchart(bvarray::Array{Int64,1}, wvarray::Array{Int64,1}, avarray::Array{Float64,1}, crossprob::Float64, mutprob::Float64)
+  n       = length(bvarray)
+  ymax    = maximum(bvarray)+5
+  xmax    = n+5
+  df1     = DataFrame(x=1:n, y=bvarray, Wartość="największa")
+  df2     = DataFrame(x=1:n, y=avarray, Wartość="średnia")
+  df3     = DataFrame(x=1:n, y=wvarray, Wartość="najmniejsza")
+  df      = vcat(df1, df2, df3)
+  suffix  = Dates.format(now(), "yyyy-mm-dd_HH:MM:SS")
 
-  println("\nAVG")
-  println(avarray)
+  myplot = plot(df, x=:x, y=:y, color=:Wartość, Geom.point, Geom.line, Guide.xlabel("Liczba populacji"), Guide.ylabel("Wartość rozwiązania"), Coord.cartesian(xmin=0, xmax=xmax, ymin=0, ymax=ymax))
 
+  draw(SVG("out/knapsack__c_"* string(crossprob) *"_m_"* string(mutprob) *"__"* suffix *".svg", 1200px, 900px), myplot)
 end
-
-# TODO
-#=
-  plot([[1, 3, 5], [2, 4, 6]], x=Row.index, y=Col.value, color=Col.index, Geom.points))
-=#
